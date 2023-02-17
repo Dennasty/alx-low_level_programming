43c1d53e982746a158f715f51efcf225e6142df3
dennasty <rocrejonuoha@gmail.com>
2023-02-17 14:35:34 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return: 1 (Failure)
- */
+* main - A C program that prints a line
+* Return: 1 (Failure)
+*/
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
