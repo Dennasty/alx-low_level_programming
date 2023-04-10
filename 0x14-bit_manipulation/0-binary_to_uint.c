a78176984fb2f3039af7e0b9f4d9514e1f20de95
“dennasty“ <rocrejonuoha@gmail.com>
2023-04-10 10:40:40 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,22 @@
  * Return: the converted number, or 0 if there is one or more
  * chars in the string b that is not 0 or 1, or b is NULL
  */
-
- unsigned int binary_to_uint(const char *b)
- {
- 	if (b == NULL)
-		return 0;
-
+unsigned int binary_to_uint(const char *b)
+{
 	unsigned int result = 0;
-	for (int i = 0; b[i] != '\0'; ++i)
+
+	if (b == NULL)
+		return (0);
+
+	for (; *b != '\0'; b++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return 0;
-		result = (result << 1) | (b[i] - '0');
+		if (*b == '0')
+			result = (result << 1);
+		else if (*b == '1')
+			result = (result << 1) | 1;
+		else
+			return (0);
 	}
 
-	return result;
+	return (result);
 }
