d798f994440f0a4f0fb7c0e707845daad09b188e
“dennasty“ <rocrejonuoha@gmail.com>
2023-04-10 19:16:49 -0700

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,53 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include "main.h"
-#include <stdio.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: The name of the file to read.
+ * @letters: The maximum number of bytes to read and print.
+ *
+ * Return: The actual number of bytes read and printed, or 0 on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
 	if (filename == NULL)
-		return 0;
+		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
-
-	char *buf = malloc(letters * sizeof(char));
-	if (buf == NULL)
-		return 0;
-
-	ssize_t bytes_read = read(fd, buf, letters);
-	if (bytes_read == -1)
 	{
-		free(buf);
-		close(fd);
-		return 0;
+		free(buffer);
+		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	free(buf);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
 	close(fd);
 
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		return 0;
-
-	return bytes_read;
+	return (bytes_written);
 }
