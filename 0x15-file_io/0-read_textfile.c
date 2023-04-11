76f06e1e907185a0277ddc31e0a105dee53111da
“dennasty“ <rocrejonuoha@gmail.com>
2023-04-10 17:38:39 -0700

    ox15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename == NULL)
 		return 0;
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return 0;
 
-    char *buf = malloc(letters * sizeof(char));
-    if (buf == NULL)
-        return 0;
+	char *buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return 0;
 
-    ssize_t bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+	ssize_t bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return 0;
+	}
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    free(buf);
-    close(fd);
+	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	free(buf);
+	close(fd);
 
-    if (bytes_written == -1 || bytes_written != bytes_read)
-        return 0;
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return 0;
 
-    return bytes_read;
+	return bytes_read;
 }
-
